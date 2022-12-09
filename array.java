ffe54defcce742e15b607fc3a66130a474398a3a
erasyl0405 <120212136+erasyl0405@users.noreply.github.com>
2022-12-10 00:37:48 +0600

    Add files via upload

---
+++ array.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class array{
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hobby: _______");
+        System.out.println("How much do you like ICT(from 1-10)): _______");
+        System.out.println("Rate this task from 1 to 10: _______");
+        System.out.println("Am I the best teacher?: _______");
+        System.out.println("My favorite color?: _______");
+    }}
